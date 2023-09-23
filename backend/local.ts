e43cb6f4d7e6f971eb36ae8cbcd0a6413f826085
Satrio <satrio@satrioadi.com>
2023-09-23 13:30:08 +0700

    feat: change local script implementation

--- backend/local.ts
+++ backend/local.ts
@@ -1,6 +1,8 @@
-import { buildApp } from "./app";
+import {buildApp} from "./app";
 
-const app = buildApp();
+const build = buildApp();
 const port = parseInt(process.env.EXPRESS_PORT ?? "3000");
 
-app.listen(port, () => console.log(`Backend listening on port ${port}`));
+build.then((app) => {
+  app.listen(port, () => console.log(`Backend listening on port ${port}`));
+});
